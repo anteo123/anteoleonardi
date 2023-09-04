5b9ab21df9b80690e7e39478b163ff81a458931d
anteo123 <anteo_leonardi07@hotmail.com>
2023-09-04 20:50:34 +0100

    css + html + js
    adding tags

---
+++ script.js
@@ -0,0 +1,11 @@
+let typed = new Typed(".auto-typed", {
+  strings: ["Front-End Developer", "Gamer", "Gym Enthusiast"],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true,
+});
+
+AOS.init({
+  duration: 1500,
+  mirror: true,
+});
